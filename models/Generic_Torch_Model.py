468a6e8708001b077a7a9c86e830c4b78654923c
RemiG3 <remig645@gmail.com>
2022-01-11 12:36:29 +0100

    Adding MWE NN model + NN-CRF model

--- models/Generic_Torch_Model.py
+++ models/Generic_Torch_Model.py
@@ -42,32 +42,257 @@ def train_test_split_sentences(X, y, test_size=0.3, random_state=0, shuffle=True
     return X, X_val, y, y_val
 
 
-def write_data(X_test, y_test, filename):
+def combine_sst_mwe(X, y_sst, y_mwe):
+    new_y_sst, new_y_mwe = y_sst.copy(), y_mwe.copy()
+    is_first = True
+    last_b = -1
+    last_B = -1
+    seq_sst_low = []
+    seq_mwe_low = []
+    nb_low = 0
+    seq_sst_upp = []
+    seq_mwe_upp = []
+    #print(len(X))
+    #print(len(y_sst))
+    #print(len(y_mwe))
+    i = 0
+    for xi, yi_sst, yi_mwe in zip(X, y_sst, y_mwe):
+        #print(i)
+        #if (xi[1] == '<eos>'):# or (yi_test == '<eos>'):
+        #    continue
+        
+        if (not is_first) and (xi[0] == 1):
+            seq_sst_low = []
+            seq_mwe_low = []
+            seq_sst_upp = []
+            seq_mwe_upp = []
+            last_b = -1
+            last_B = -1
+            nb_low = 0
+        
+        #print(yi_mwe)
+        if yi_mwe in ['<eos>', '', 'O'] or (xi[1] == '<eos>'):
+            if (xi[1] == '<eos>'):
+                new_y_mwe[i] = '<eos>'
+            #print(yi_mwe, seq_mwe_upp)
+            if len(seq_sst_upp) > 1:
+                new_y_mwe[last_B] = 'B'
+                #print(i, seq_mwe_upp, seq_mwe_low, new_y_mwe)
+                n = 0
+                for j in range(1, len(seq_sst_upp)+nb_low):
+                    if (nb_low == 0) or ((nb_low > 0) and (not new_y_mwe[last_B+j].islower())):
+                        new_y_mwe[last_B+j] = 'I'
+                        new_y_sst[last_B+j] = ''
+                        if(seq_sst_upp[0] == ''):
+                            seq_sst_upp[0] = seq_sst_upp[j-n]
+                    else:
+                        n += 1
+                new_y_sst[last_B] = seq_sst_upp[0]
+                for j in range(1, len(seq_sst_upp)+nb_low):
+                    if (nb_low == 0) or ((nb_low > 0) and (not new_y_mwe[last_B+j].islower())):
+                        new_y_sst[last_B+j] = ''
+            elif (len(seq_sst_upp) == 1):
+                new_y_mwe[i-1] = 'O'
+            
+            if len(seq_sst_low) > 1:
+                new_y_mwe[last_b] = 'b'
+                for j in range(1, len(seq_sst_low)):
+                    new_y_mwe[last_b+j] = 'i'
+                    if(seq_sst_low[0] == ''):
+                        seq_sst_low[0] = seq_sst_low[j]
+                new_y_sst[last_b] = seq_sst_low[0]
+                for j in range(1, len(seq_sst_low)):
+                    new_y_sst[last_b+j] = ''
+            elif (len(seq_sst_upp) == 1):
+                new_y_mwe[i-1] = 'O'
+            
+            seq_sst_low = []
+            seq_mwe_low = []
+            seq_sst_upp = []
+            seq_mwe_upp = []
+            last_b = -1
+            last_B = -1
+            nb_low = 0
+        elif yi_mwe == 'B':
+            if (last_b != -1):
+                if len(seq_sst_low) > 1:
+                    new_y_mwe[last_b] = 'b'
+                    for j in range(1, len(seq_sst_low)):
+                        new_y_mwe[last_b+j] = 'i'
+                        if(seq_sst_low[0] == ''):
+                            seq_sst_low[0] = seq_sst_low[j]
+                    new_y_sst[last_b] = seq_sst_low[0]
+                    for j in range(1, len(seq_sst_low)):
+                        new_y_sst[last_b+j] = ''
+                elif (len(seq_sst_low) == 1):
+                    new_y_mwe[i-1] = 'o'
+                seq_sst_low = []
+                seq_mwe_low = []
+            
+            if (last_B != -1) and (nb_low == 0):
+                if len(seq_sst_upp) > 1:
+                    new_y_mwe[last_B] = 'B'
+                    for j in range(1, len(seq_sst_upp)):
+                        new_y_mwe[last_B+j] = 'I'
+                        if(seq_sst_upp[0] == ''):
+                            seq_sst_upp[0] = seq_sst_upp[j]
+                    new_y_sst[last_B] = seq_sst_upp[0]
+                    for j in range(1, len(seq_sst_upp)):
+                        new_y_sst[last_B+j] = ''
+                elif (len(seq_sst_upp) == 1):
+                    if not new_y_mwe[i-1].islower():
+                        new_y_mwe[i-1] = 'O'
+                seq_sst_upp = []
+                seq_mwe_upp = []
+                last_B = -1
+                #new_y_mwe[i] = 'I'
+                #yi_mwe = 'I'
+            #elif (last_B != -1) and (last_b != -1):
+            #    new_y_mwe[i] = 'I'
+            #    yi_mwe = 'I'
+            
+            if (last_B == -1):
+                last_B = i
+            seq_sst_upp.append(yi_sst)
+            seq_mwe_upp.append(yi_mwe)
+        elif yi_mwe == 'I':
+            if (last_b != -1):
+                if len(seq_sst_low) > 1:
+                    new_y_mwe[last_b] = 'b'
+                    for j in range(1, len(seq_sst_low)):
+                        new_y_mwe[last_b+j] = 'i'
+                        if(seq_sst_low[0] == ''):
+                            seq_sst_low[0] = seq_sst_low[j]
+                    new_y_sst[last_b] = seq_sst_low[0]
+                    for j in range(1, len(seq_sst_low)):
+                        new_y_sst[last_b+j] = ''
+                elif (len(seq_sst_low) == 1):
+                    new_y_mwe[i-1] = 'o'
+                seq_sst_low = []
+                seq_mwe_low = []
+            
+            if last_B == -1:
+                last_B = i
+            seq_sst_upp.append(yi_sst)
+            seq_mwe_upp.append(yi_mwe)
+        elif yi_mwe == 'b':
+            if (last_b != -1):
+                if len(seq_sst_low) > 1:
+                    new_y_mwe[last_b] = 'b'
+                    for j in range(1, len(seq_sst_low)):
+                        new_y_mwe[last_b+j] = 'i'
+                        if(seq_sst_low[0] == ''):
+                            seq_sst_low[0] = seq_sst_low[j]
+                    new_y_sst[last_b] = seq_sst_low[0]
+                    for j in range(1, len(seq_sst_low)):
+                        new_y_sst[last_b+j] = ''
+                elif (len(seq_sst_low) == 1):
+                    new_y_mwe[i-1] = 'o'
+                seq_sst_low = []
+                seq_mwe_low = []
+            last_b = i
+            seq_sst_low.append(yi_sst)
+            seq_mwe_low.append(yi_mwe)
+            nb_low += 1
+        elif yi_mwe == 'i':
+            if last_b == -1:
+                last_b = i
+            seq_sst_low.append(yi_sst)
+            seq_mwe_low.append(yi_mwe)
+            nb_low += 1
+        elif yi_mwe == 'o':
+            if len(seq_sst_low) > 1:
+                new_y_mwe[last_b] = 'b'
+                for j in range(1, len(seq_sst_low)):
+                    new_y_mwe[last_b+j] = 'i'
+                    if(seq_sst_low[0] == ''):
+                        seq_sst_low[0] = seq_sst_low[j]
+                new_y_sst[last_b] = seq_sst_low[0]
+                for j in range(1, len(seq_sst_low)):
+                    new_y_sst[last_b+j] = ''
+            elif (len(seq_sst_low) == 1):
+                if (last_b == -1):
+                    new_y_mwe[i-1] = 'O'
+                else:
+                    new_y_mwe[i-1] = 'o'
+            else:
+                new_y_mwe[i] = 'O'
+                
+                if len(seq_sst_upp) > 1:
+                    new_y_mwe[last_B] = 'B'
+                    #print(i, seq_mwe_upp, seq_mwe_low, new_y_mwe)
+                    n = 0
+                    for j in range(1, len(seq_sst_upp)+nb_low):
+                        if (nb_low == 0) or ((nb_low > 0) and (not new_y_mwe[last_B+j].islower())):
+                            new_y_mwe[last_B+j] = 'I'
+                            new_y_sst[last_B+j] = ''
+                            if(seq_sst_upp[0] == ''):
+                                seq_sst_upp[0] = seq_sst_upp[j-n]
+                        else:
+                            n += 1
+                    new_y_sst[last_B] = seq_sst_upp[0]
+                    for j in range(1, len(seq_sst_upp)+nb_low):
+                        if (nb_low == 0) or ((nb_low > 0) and (not new_y_mwe[last_B+j].islower())):
+                            new_y_sst[last_B+j] = ''
+                elif (len(seq_sst_upp) == 1):
+                    new_y_mwe[i-1] = 'O'
+                
+                last_B = -1
+                seq_sst_upp = []
+                seq_mwe_upp = []
+                nb_low = -1
+                
+            nb_low += 1
+            seq_sst_low = []
+            seq_mwe_low = []
+            last_b = -1
+        
+        is_first = False
+        i += 1
+    
+    return new_y_sst, new_y_mwe
+
+
+def write_data(X_test, y_sst, y_mwe, filename):
     #xi_test format: num, word, lem, POS
     with open(filename, 'w', encoding='utf-8') as f:
         is_first = True
-        for xi_test, yi_test in zip(X_test, y_test):
-            if (xi_test[1] == '<eos>'):# or (yi_test == '<eos>'):
+        num_prev_mwe = 1
+        num_prev_mwe_lower = 1
+        for xi, yi_sst, yi_mwe in zip(X_test, y_sst, y_mwe):
+            if (xi[1] == '<eos>'):# or (yi_test == '<eos>'):
                 continue
-            if (not is_first) and xi_test[0] == 1:
+            if (not is_first) and (xi[0] == 1):
+                num_prev_mwe = 1
+                num_prev_mwe_lower = 1
                 f.write('\n')
-            yi_test = yi_test if yi_test != '<eos>' else ''
-            f.write(f'{xi_test[0]}\t{xi_test[1]}\t-\t{xi_test[3]}\tO\t0\t\t{yi_test}\n')
+            yi_sst_ = yi_sst if yi_sst != '<eos>' else ''
+            yi_mwe_ = yi_mwe if (yi_mwe != '<eos>' and yi_mwe != '') else 'O'
+            num_mwe = num_prev_mwe if yi_mwe_ == 'I' else 0
+            num_mwe = num_prev_mwe_lower if yi_mwe_ == 'i' else num_mwe
+            
+            f.write(f'{xi[0]}\t{xi[1]}\t-\t{xi[3]}\t{yi_mwe_}\t{num_mwe}\t\t{yi_sst_}\n')
+
             is_first = False
+            if not yi_mwe.islower():
+                num_prev_mwe = xi[0]
+            num_prev_mwe_lower = xi[0]
         f.write('\n')
 
 
 def extract_data(data_in):
-    X, y = [], []
+    X, y_mwe, y_sst = [], [], []
     for i, line in enumerate(data_in):
         if line != '\n':
             num, word, lem, pos, mwe, _, _, supersenses, _ = line[:-1].split('\t')
             X.append([int(num), word, lem, pos])
-            y.append(supersenses)
+            y_sst.append(supersenses)
+            y_mwe.append(mwe)
         else :
             X.append('')
-            y.append('')
-    return X, y
+            y_sst.append('')
+            y_mwe.append('')
+    return X, y_sst, y_mwe
 
 
 def encode_sentence(le_vocab, int_texts, int_labels, max_len, use_pos):
@@ -170,7 +395,7 @@ def transform_test(le_vocab, le, X, y=None, max_len=16, le_pos=None):
     return X_test, Y_test
 
 
-def align_pred(X_orig, y_pred, le):
+def align_pred(X_orig, y_pred, le, is_mwe):
     y_trf_pred = []
     i_y = 0
     i = 0
@@ -183,7 +408,7 @@ def align_pred(X_orig, y_pred, le):
             i = 0
         
         if i >= max_i:
-            next_pred = le['']
+            next_pred = le['O'] if is_mwe else le['']
         else:
             next_pred = y_pred[i_y][i].item()
         
@@ -224,7 +449,7 @@ class Simple_GRU(nn.Module):
 
 
 
-class NN_Model(Model): # Compatible with torch model only
+class NN_Model(Model): # Compatible with torch models only
     def __init__(self, model, criterion=nn.CrossEntropyLoss(), optim=optim.Adam):
         super().__init__()
         self.model = model
@@ -303,4 +528,103 @@ class NN_Model(Model): # Compatible with torch model only
         f1_score = 2*precision*recall / (precision + recall) if (precision + recall) != 0 else 0.
         accuracy = accuracy/nb_tokens
         return precision, recall, f1_score, accuracy
+
+
+class NN_CRF_Model(Model): # Compatible with torch+torchCRF models only
+    def __init__(self, nn_model, crf_model, optim=optim.Adam):
+        super().__init__()
+        self.nn_model = nn_model
+        self.crf_model = crf_model
+        self.optim = optim
+
+    def fit(self, train_loader, valid_loader, epochs):
+        all_parameters = list(self.nn_model.parameters()) + list(self.crf_model.parameters())
+        optimizer = self.optim(filter(lambda param: param.requires_grad, all_parameters))
+        for epoch in range(epochs):
+            self.nn_model.train()
+            self.crf_model.train()
+            total_loss = num = 0
+            for x, y in train_loader:
+                optimizer.zero_grad()
+                mask = (y != 0)
+                emissions = self.nn_model(x)
+                loss = -self.crf_model(emissions, y, mask)
+                loss.backward()
+                optimizer.step()
+                total_loss += loss.item()
+                num += len(y)
+            print(epoch+1, total_loss / num, *self.perf(valid_loader))
+    
+    def perf(self, loader):
+        def num_correct(y_pred, y):
+            correct = 0
+            for y_pred_current, y_current in zip(y_pred, y):
+                for i in range(len(y_pred_current)):
+                    if y_pred_current[i] == y_current[i]:
+                        correct += 1
+            return correct
+
+        self.nn_model.eval()
+        self.crf_model.eval()
+        loss = num_loss = correct = num_perf = 0
+        for x, y in loader:
+            with torch.no_grad():
+                mask = (y != 0)
+                emissions = self.nn_model(x)
+                y_pred = self.crf_model.decode(emissions, mask)
+                correct += num_correct(y_pred, y)
+                num_perf += torch.sum(mask).item()
+                loss -= self.crf_model(emissions, y, mask).item()
+                num_loss += len(y)
+        return loss / num_loss, correct / num_perf
+
+    def predict(self, X_test):
+        self.nn_model.eval()
+        #return torch.max(self.model(X_test), 2)[1]
+        #print(X_test.size())
+        y_pred = self.nn_model(X_test)
+        #print(y_pred.size())
+        mask = (torch.max(y_pred, 2)[1] != 0)
+        #print(mask.size())
+        return torch.tensor(self.crf_model.decode(y_pred, mask=mask))
+
+    def score(self, X_test, y_test, exception_class, le):
+        #return super().score(X_test, y_test, exception_class)
+        """
+        Returns:
+            precision = #(correct)/#(predicted)
+            recall = #(correct)/#(gold)
+            F1_score = 2*precision*recall / (precision + recall)
+            accuracy = #(correct: supersense or no-supersense)/#(tokens)
+        """
+        
+        y_hat = self.predict(X_test)
+        
+        y_test = y_test.view((-1,))
+        y_hat = y_hat.view((-1,))
+        #print(len(y_test))
+
+        nb_predicted, nb_gold, nb_tokens = 0, 0, 0
+        nb_correct, accuracy = 0, 0
+        for yi, yi_hat in zip(y_test, y_hat):
+            yi, yi_hat = yi.item(), yi_hat.item()
+            #print(yi_hat, yi)
+            if yi != le['<eos>']:
+                yi_hat = yi_hat if yi_hat != le['<eos>'] else le['']
+                if yi_hat == yi:
+                    accuracy += 1
+                if yi_hat != exception_class:
+                    nb_predicted += 1
+                if yi != exception_class:
+                    nb_gold += 1
+                if (yi != exception_class) and (yi == yi_hat):
+                    nb_correct += 1
+                nb_tokens += 1
+
+        precision = nb_correct/nb_predicted if nb_predicted != 0 else 0.
+        recall = nb_correct/nb_gold if nb_gold != 0 else 0.
+        f1_score = 2*precision*recall / (precision + recall) if (precision + recall) != 0 else 0.
+        accuracy = accuracy/nb_tokens
+        return precision, recall, f1_score, accuracy
+
 
